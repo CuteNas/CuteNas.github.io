4180873b3ac01e035bc56f91f27db4c4358b2735
CuteNas <142705375+CuteNas@users.noreply.github.com>
2024-03-19 21:44:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var S = {
         S.Drawing.init('.canvas');
         document.body.classList.add('body--ready');
             //想说什么
-            S.UI.simulate("祝你|生日快乐哟|#countdown 3|#rectangle 15x15|#circle 12 |#time");
+            S.UI.simulate("曲国强|祝你|生日快乐哟|#countdown 3|#rectangle 15x15|#circle 12 |#time");
             S.Drawing.loop(function () {
                 S.Shape.render();
             });
